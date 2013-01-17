35e0e3b28115458bf5ee1a033be87cdc46d5d814
Scumm Boy <scumm.boy@gmail.com>
2013-01-17 12:26:08 -0500

    Fixed spacing and potential loop fubar

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 var http = require('http'), url = require('url');
 
 function handleServerRequest(request, response) {
-    //console.log("Client hit %s, with the following: ", request.url, request.headers);
+  //console.log("Client hit %s, with the following: ", request.url, request.headers);
 
-    var urlInfo = url.parse(request.url, true);
+  var urlInfo = url.parse(request.url, true);
 
-    // Log the value of bork and blag
-    console.log("1st Query info: ", urlInfo.query.bork);
-    console.log("2nd Query Info: ", urlInfo.query.blag);
+  // Log the value of bork and blag
+  console.log("1st Query info: ", urlInfo.query.bork);
+  console.log("2nd Query Info: ", urlInfo.query.blag);
 
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write('Hello World\n');
-    if (urlInfo.query.bork !== undefined) {
-        response.write(urlInfo.query.bork);
-        response.write('\n');
-    }
-    if (urlInfo.query.blag !== undefined) {
-        response.write(urlInfo.query.blag);
-        response.end('\n');
-    }
+  response.writeHead(200, {'Content-Type':'text/plain'});
+  response.write('Hello World\n');
+  if (urlInfo.query.bork !== undefined) {
+    response.write(urlInfo.query.bork);
+    response.write('\n');
+  }
+  if (urlInfo.query.blag !== undefined) {
+    response.write(urlInfo.query.blag);
+  }
+  response.end('\n');
 }
 
 var httpServer = http.createServer(handleServerRequest);
